8213bdbdb1b2ac5106fd0e904c66b45ee1295362
Renzo Carpio <me@renzocarpio.com>
2020-01-03 18:00:03 -0500

    contar todos y mostrar en cabecera

--- src/components/ListTodo/ListTodo.js
+++ src/components/ListTodo/ListTodo.js
@@ -7,6 +7,9 @@ export default function ListTodo(props) {
   const { allTodos } = props;
   return (
     <div className="list-todo">
+      <div class="list-header">
+        <span>All: {allTodos.length}</span>
+      </div>
       {allTodos.map((todo, index) => (
         <Todo key={index} todo={todo} />
       ))}
